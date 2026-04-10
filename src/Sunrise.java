b641841db209d074a6f347e9e537633bf8514667
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-10 01:43:51 -0700

    Update Sunrise.java

--- src/Sunrise.java
+++ src/Sunrise.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Sunrise {
-   
-        // TODO: Read n
-        // TODO: Read the array of heights
-        // TODO: Iterate through the array and count how many buildings are taller than the current maximum height encountered so far.
-        // TODO: Print the count
-    
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            if (!scanner.hasNextInt()) return;
+            int n = scanner.nextInt();
+            if (n <= 0) {
+                System.out.println(0);
+                return;
+            }
+            int[] heights = new int[n];
+            for (int i = 0; i < n; i++) {
+                if (scanner.hasNextInt()) {
+                    heights[i] = scanner.nextInt();
+                }
+            }
+
+            int count = 1;
+            int maxHeight = heights[0];
+
+            for (int i = 1; i < n; i++) {
+                if (heights[i] > maxHeight) {
+                    count++;
+                    maxHeight = heights[i];
+                }
+            }
+            System.out.println(count);
+        }
+    }
 }
